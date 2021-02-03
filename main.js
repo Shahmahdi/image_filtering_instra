09f085263ef8766512c895a8fd4e6af01b0c352a
Shahmahdi <shahmahdi11@gmail.com>
2021-02-03 14:11:48 +0600

    red and green filter added

--- main.js
+++ main.js
@@ -10,17 +10,17 @@ const brightness = document.getElementById("brightness");
 const contrast = document.getElementById("contrast");
 const grayscale = document.getElementById("grayscale");
 
-const selectedImage = new Image;
+const selectedImage = new Image();
+
+let imageData = null;
+let originalPixels = null;
+let currentPixels = null;
 
 imageInput.onchange = e => {
   if (e.target.files && e.target.files[0]) {
     selectedImage.src = URL.createObjectURL(e.target.files[0]);
   }
 };
-
-let imageData = null;
-let originalPixels = null;
-let currentPixels = null;
 
 selectedImage.onload = () => {
   canvas.width = selectedImage.width;
@@ -34,10 +34,24 @@ const clampRGBValue = value => Math.max(0, Math.min(Math.floor(value), 255));
 
 const getIndex = (x, y) => (x + y * selectedImage.width) * 4;
 
-const B_OFFSET = 2;
+const RED_OFFSET = 0;
+const GREEN_OFFSET = 1;
+const BLUE_OFFSET = 2;
+
+const addRed = (x, y, value) => {
+  const index = getIndex(x, y) + RED_OFFSET;
+  const currentValue = currentPixels[index];
+  currentPixels[index] = clampRGBValue(currentValue + value);
+};
+
+const addGreen = (x, y, value) => {
+  const index = getIndex(x, y) + GREEN_OFFSET;
+  const currentValue = currentPixels[index];
+  currentPixels[index] = clampRGBValue(currentValue + value);
+};
 
 const addBlue = (x, y, value) => {
-  const index = getIndex(x, y) + B_OFFSET;
+  const index = getIndex(x, y) + BLUE_OFFSET;
   const currentValue = currentPixels[index];
   currentPixels[index] = clampRGBValue(currentValue + value);
 };
@@ -45,12 +59,15 @@ const addBlue = (x, y, value) => {
 const runPipeline = () => {
   currentPixels = originalPixels.slice();
 
+  const redFilterInputValue = Number(red.value);
+  const greenFilterInputValue = Number(green.value);
   const blueFilterInputValue = Number(blue.value);
 
   for (let i = 0; i < selectedImage.height; i++) {
     for (let j = 0; j < selectedImage.width; j++) {
       // Add blue to pixel (j, i) according to selected value
-
+      addRed(j, i, redFilterInputValue);
+      addGreen(j, i, greenFilterInputValue);
       addBlue(j, i, blueFilterInputValue);
     }
   }
@@ -63,10 +80,10 @@ const runPipeline = () => {
 };
 
 red.onchange = runPipeline;
-blue.onchange = runPipeline;
 green.onchange = runPipeline;
+blue.onchange = runPipeline;
+brightness.onchange = runPipeline;
 grayscale.onchange = runPipeline;
 contrast.onchange = runPipeline;
-brightness.onchange = runPipeline;
 
 
